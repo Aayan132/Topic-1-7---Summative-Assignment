811a5ed91e0ed56e1f262ae4f62dcd51af12b413
Aayan Shaikh <363096132@GoTVDSB.ca>
2026-03-10 13:17:40 -0400

    ALMOST DONE

--- Topic 1-7 - Summative Assignment/Program.cs
+++ Topic 1-7 - Summative Assignment/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Design;
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace Topic_1_7___Summative_Assignment
 {
@@ -8,20 +9,23 @@ namespace Topic_1_7___Summative_Assignment
         {
 
             Random random = new Random();
-            //string userschoice = "";
-            //string computerschoice = "";
-            string rulesoption = "";
-            int bank = 100;
-            int bet = 0;
-            //int win = 0;
-            //int losses = 0;
-            //int ties = 0;
+            //string userschoice;
+            string rulesoption;
+            double bank = 100;
+            double bet;
+            string computerchoice;
+            bool done;
+            int win = 0;
+            int losses = 0;
+            int ties = 0;
+
+            DrawRock();
 
 
             Console.WriteLine("Welcome to the Rock Paper Scissors Casino");
             Console.WriteLine();
             Console.WriteLine("You start with $100");
-            Console.WriteLine("Do you want to see the rules?");
+            Console.WriteLine("Do you want to see the rules? (type Yes or No)");
 
             rulesoption = Console.ReadLine();
 
@@ -33,8 +37,8 @@ namespace Topic_1_7___Summative_Assignment
                 Console.WriteLine("3: Rock beats Scissors");
                 Console.WriteLine("4: Scissors beats Papper");
                 Console.WriteLine("5: Papper beats Rock");
-                Console.WriteLine("6: If you win you double your money");
-                Console.WriteLine("7: If you lose you lose your money");
+                Console.WriteLine("6: If you win you win the amount you bet");
+                Console.WriteLine("7: If you lose you lose the money you bet");
                 Console.WriteLine("8: If you tie your money stays the same");
                 Console.WriteLine();
 
@@ -48,12 +52,12 @@ namespace Topic_1_7___Summative_Assignment
             }
 
 
-            bool done = false;
+            done = false;
             while (!done)
             {
                 Console.WriteLine("You have $" + bank);
                 Console.WriteLine("Enter your bet!");
-                Int32.TryParse(Console.ReadLine(), out bet);
+                double.TryParse(Console.ReadLine(), out bet);
                 Console.WriteLine();
 
                 if (bet > bank)
@@ -71,14 +75,90 @@ namespace Topic_1_7___Summative_Assignment
                 {
                     Console.WriteLine();
                     Console.WriteLine("Thank you for playing!");
+                    Console.WriteLine("Game over");
+                    Console.WriteLine("Final Bank " + bank);
+                    Console.WriteLine("Wins " + win);
+                    Console.WriteLine("Losses " + losses);
+                    Console.WriteLine("Ties " + ties);
                     break;
                 }
 
-                
+                int number = random.Next(1, 4);
 
-              }
+                if (number == 1)
+                {
+                    computerchoice = "rock";
+                }
+
+                else if (number == 2)
+                {
+                    computerchoice = "paper";
+                }
+
+                else
+                {
+                    computerchoice = "scissors";
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("You chose " + userschoice);
+                Console.WriteLine("The compuer chose " + computerchoice);
+                Console.WriteLine();
+
+                if (userschoice == computerchoice)
+                {
+                    Console.WriteLine("You picked the same thing TIE");
+                    ties = ties + 1;
+                }
+
+                else if (userschoice == "rock" && computerchoice == "scissors" 
+                    || userschoice == "paper" && computerchoice == "rock" 
+                    || userschoice == "scissors" && computerchoice == "paper")
+                {
+                    Console.WriteLine("you win!");
+                    win = win + 1;
+                    bank = bank + bet;
+                }
+
+                else
+                {
+                    Console.WriteLine("You lose😔");
+                    losses = losses + 1;
+                    bank = bank - bet;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Wins " + win);
+                Console.WriteLine("Losses " + losses);
+                Console.WriteLine("Ties " + ties);
+                Console.WriteLine();
+
+                if (bank <= 0)
+                {
+                    Console.WriteLine("You ran out of money");
+                    done = true;
+                }
+
+
+
             }
         }
+        public static void DrawRock()
+        {
+            Console.WriteLine(@"
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)"
+);
+
+
+        }
+
     }
+}
+
 
 
